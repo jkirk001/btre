780e78f1c8b5b84d4da898fa9564bce741581816
Jon Kirkpatrick <jonkirkpatrick@jons-mbp.attlocal.net>
2020-11-15 17:12:09 -0800

    updated settings

--- btre/settings.py
+++ btre/settings.py
@@ -142,13 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'test.btre.jon@gmail.com'
-EMAIL_HOST_PASSWORD = 'hwpxusyxdnedquwu'
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
